149fd92bbfd0972ffaa7eff913bfb4b40f78af6b
weixingyu <weixingyu@360.cn>
2025-07-02 15:55:41 +0800

    使用map优化查询速度 精简部分代码与逻辑

--- stalker_trace_so.py
+++ stalker_trace_so.py
@@ -10,10 +10,10 @@ import idautils
 import idc
 import random
 from functools import reduce
+import json
 
 template_js = '''
-var func_addr = [[func_addr]];
-var func_name = [[func_name]];
+var func_addr_map = [func_addr_map];
 var so_name = "[so_name]";
 
 /*
@@ -30,29 +30,27 @@ var print_stack = false;
 var print_stack_mode = "FUZZY";
 
 function addr_in_so(addr){
-    for (const module of Process.enumerateModules()) {
-        if (addr.compare(module.base) >= 0 && addr.compare(module.base.add(module.size)) < 0) {
-            console.log(addr.toString(16), "is in", module.name, "offset: 0x" + addr.sub(module.base).toString(16));
-        }
+    var module = Process.getModuleByName(so_name);
+    if (addr.compare(module.base) >= 0 && addr.compare(module.base.add(module.size)) < 0) {
+        console.log(addr.toString(16), "is in", module.name, "offset: 0x" + addr.sub(module.base).toString(16));
     }
 }
+
 function hook_dlopen() {
     Interceptor.attach(Process.getModuleByName('libc.so').getExportByName('android_dlopen_ext'),
         {
             onEnter: function (args) {
+                this.is_can_hook = false;
                 var pathptr = args[0];
                 if (pathptr !== undefined && pathptr != null) {
-                    var path = ptr(pathptr).readCString();
-                    //console.log(path);
-                    if (path.indexOf(so_name) >= 0) {
+                    var path = pathptr.readCString();
+                    if (path.includes(so_name)) {
                         this.is_can_hook = true;
                     }
                 }
             },
             onLeave: function (retval) {
                 if (this.is_can_hook) {
-                    // note: you can do any thing before or after stalker trace so.
-
                     trace_so();
                 }
             }
@@ -60,14 +58,16 @@ function hook_dlopen() {
     );
 }
 
+var globalCallCount = 0;
+
 function trace_so(){
-    var times = 1;
     var module = Process.getModuleByName(so_name);
     var pid = Process.getCurrentThreadId();
+    var libc = Process.getModuleByName("libc.so");
     console.log("start Stalker!");
     Stalker.exclude({
-        "base": Process.getModuleByName("libc.so").base,
-        "size": Process.getModuleByName("libc.so").size
+        "base": libc.base,
+        "size": libc.size
     })
     Stalker.follow(pid,{
         events:{
@@ -77,49 +77,38 @@ function trace_so(){
             block:false,
             compile:false
         },
-        onReceive:function(events){
-        },
+        onReceive:function(events){},
         transform: function (iterator) {
-            var instruction = iterator.next();
-            do{
-                if (func_addr.indexOf(instruction.address - module.base) !== -1) {
-                    console.log("call" + times + ":" + func_name[func_addr.indexOf(instruction.address - module.base)])
-                    times = times + 1
+            let instruction;
+            while ((instruction = iterator.next()) !== null) {
+                let offset = instruction.address.sub(module.base).toInt32();
+                let name = func_addr_map[offset];
+                if (name !== undefined) {
+                    globalCallCount += 1;
+                    console.log("call" + globalCallCount + ":" + name);
                     if (print_stack) {
-                        if (print_stack_mode === "FUZZY") {
-                            iterator.putCallout((context) => {
-                                console.log("backtrace:\\n"+Thread.backtrace(context, Backtracer.FUZZY).map(DebugSymbol.fromAddress).join('\\n'));
-                                console.log('---------------------')
-                            });
-                        }
-                        else if (print_stack_mode === "ACCURATE") {
-                            iterator.putCallout((context) => {
-                                console.log("backtrace:\\n"+Thread.backtrace(context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\\n'));
-                                console.log('---------------------')
-                            })
-                        }
-
-                        else if (print_stack_mode === "MANUAL") {
-                            iterator.putCallout((context) => {
-                                console.log("backtrace:")
+                        iterator.putCallout((context) => {
+                            console.log("backtrace:\\n");
+                            if (print_stack_mode === "FUZZY") {
+                                console.log(Thread.backtrace(context, Backtracer.FUZZY).map(DebugSymbol.fromAddress).join('\\n'));
+                            } else if (print_stack_mode === "ACCURATE") {
+                                console.log(Thread.backtrace(context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\\n'));
+                            } else if (print_stack_mode === "MANUAL") {
                                 Thread.backtrace(context, Backtracer.FUZZY).map(addr_in_so);
-                                console.log('---------------------')
-                            })
-                        }
+                            }
+                            console.log('---------------------');
+                        });
                     }
                 }
                 iterator.keep();
-            } while ((instruction = iterator.next()) !== null);
+            }
         },
-
-        onCallSummary:function(summary){
-
-        }
+        onCallSummary:function(summary){}
     });
     console.log("Stalker end!");
 }
 
-setImmediate(hook_dlopen,0);
+setImmediate(hook_dlopen);
         '''
 
 
@@ -130,7 +119,7 @@ class UI_Hook(idaapi.UI_Hooks):
     def finish_populating_widget_popup(self, form, popup):
         form_type = idaapi.get_widget_type(form)
         if form_type == idaapi.BWN_FUNCS or form_type == idaapi.BWN_PSEUDOCODE or form_type == idaapi.BWN_DISASM:
-            idaapi.attach_action_to_popup(form, popup, "stalkerTraceSo:genJsScript", None)
+            idaapi.attach_action_to_popup(form, popup, "stalkerTraceSo:genJsScript", "")
 
 
 class GenerateFridaHookScript(idaapi.action_handler_t):
@@ -150,34 +139,34 @@ class GenerateFridaHookScript(idaapi.action_handler_t):
         return idaapi.AST_ENABLE_ALWAYS
 
 
-def generate_hook_code(template_js, func_addr, func_name, so_name):
+def generate_hook_code(template_js, func_addr_map, so_name):
     replacements = {
-        "[func_addr]": ', '.join(func_addr),
-        "[func_name]": ', '.join(func_name),
+        "[func_addr_map]": func_addr_map,
         "[so_name]": "%s" % so_name
     }
     return reduce(lambda acc, item: acc.replace(item[0], item[1]), replacements.items(), template_js)
 
 
 def generate_js_script(func_list):
-    func_addr = []
-    func_name = []
+    func_addr_map = {}
+    base_addr = get_imagebase()
     for func_ea in func_list:
-        # thumb mode
         if idc.get_sreg(func_ea, "T"):
-            func_addr.append(hex(func_ea + 1))
+            addr = func_ea + 1
         else:
-            func_addr.append(hex(func_ea))
-        func_name.append('"{}"'.format(idc.get_func_name(func_ea)))
-
+            addr = func_ea
+        name = '"{}"'.format(idc.get_func_name(func_ea))
+        offset = addr - base_addr
+        func_addr_map[str(offset)] = name
+    import json
+    func_addr_map_json = json.dumps(func_addr_map)
     so_path, so_name = os.path.split(ida_nalt.get_input_file_path())
-    hook_code = generate_hook_code(template_js, func_addr, func_name, so_name)
+    hook_code = generate_hook_code(template_js, func_addr_map_json, so_name)
     r = [random.choice("abcdefghijklmnopqrstuvwxyz") for _ in range(5)]
     script_name = "trace_" + so_name.split(".")[0] + '_' + ''.join(r) + ".js"
     save_path = os.path.join(so_path, script_name)
     with open(save_path, "w", encoding="utf-8") as f:
         f.write(hook_code)
-
     print("usage:")
     print(f'frida -U -l "{save_path}" -f [package name]')
 
@@ -192,8 +181,8 @@ class stalker_trace_so(plugin_t):
     def init(self):
         print("stalker_trace_so plugin has been loaded.")
         idaapi.register_action(
-            idaapi.action_desc_t("stalkerTraceSo:genJsScript", "stalker trace so", GenerateFridaHookScript(), None,
-                                 None, 201))
+            idaapi.action_desc_t("stalkerTraceSo:genJsScript", "stalker trace so", GenerateFridaHookScript(), "",
+                                 "", 201))
         # Add ui hook
         self.ui_hook = UI_Hook()
         self.ui_hook.hook()
